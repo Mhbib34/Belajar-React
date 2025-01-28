22063ab03295c389cb61d87d5024d9cc821b3d81
Mhbib34 <mhabib34official@gmail.com>
2025-01-28 23:23:09 +0700

    Mengubah placeholder

--- src/components/Element/Input/index.jsx
+++ src/components/Element/Input/index.jsx
@@ -3,7 +3,7 @@ import Input from "./Input";
 function InputIndex() {
   return (
     <div className="flex flex-col gap-2 my-4  ">
-      <Input type="email" placeholder="Enter your email..." />
+      <Input type="email" placeholder="example@gmail.com" />
       <Input type="password" placeholder="*********" />
     </div>
   );
